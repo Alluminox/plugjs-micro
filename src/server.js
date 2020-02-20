ab979081170863e667fba86d834bec499f36ab1b
Alluminox <alluminox.devellopers@gmail.com>
2020-02-20 15:53:56 -0300

    refactoring Details

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ import registry from 'plugjs-registry';
 /*
 Call Registers
 */
-// server.register(registry.register.key, registry.register.middleware)
-// server.register(registry.unregister.key, registry.unregister.middleware)
+server.register(registry.register.key, registry.register.middleware)
+server.register(registry.unregister.key, registry.unregister.middleware)
 
 export * from './api';
 export default server.start
